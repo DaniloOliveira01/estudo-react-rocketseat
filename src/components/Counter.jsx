09220c232ed0415720fe09dce4703c9768bbe4f7
Danilo Oliveira <oliveira.83555@gmail.com>
2022-01-31 22:52:44 -0300

    ..

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -12,7 +12,7 @@ export function Counter() {
     <div>
       <h2>{counter}</h2>
       <button type="buttom" onClick={increment}>
-        Increment
+        Incremente
       </button>
     </div>
   );
